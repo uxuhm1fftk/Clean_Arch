9b921d4e4c75fad028b042f59493b56bc059908f
Ali <r.barzegarr@gmail.com>
2020-02-21 21:58:16 +0330

    Initial Application Layer

---
+++ CleanArch/CleanArch.Domain/Interfaces/ICourceRepository.cs
@@ -0,0 +1,12 @@
+﻿using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Interfaces
+{
+    public interface Interface1
+    {
+        IEnumerable<Course> GetCources();
+    }
+}
